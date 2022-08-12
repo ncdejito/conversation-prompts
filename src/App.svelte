0bbfa2c4e85b780196c07a98d0f6058505ab7276
Niccolo Dejito <ncdejito@gmail.com>
2022-08-12 10:55:05 +0800

    Add custom svelte files

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,103 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+    import Hoverable from "./Hoverable.svelte";
+    import Modal from "./Modal.svelte";
+    import { prompts, sources } from "./Prompts.js";
+
+    let showModal = false;
+    let Set = "Friends"; // ["Work", "Friends", "SO"];
+
+    function getRandomInt(min, max) {
+        //The maximum is exclusive and the minimum is inclusive
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min) + min);
+    }
+
+    function getPrompt(Set) {
+        let max = Object.keys(prompts[Set]).length;
+        let ind = getRandomInt(0, max);
+        return prompts[Set][ind];
+    }
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+<Hoverable let:hovering={active}>
+    <div class:active>
+        {#if active}
+            <button
+                on:click={() => {
+                    showModal = true;
+                    Set = "Work";
+                }}
+            >
+                Small talk
+            </button>
+        {:else}
+            <p>💼 Work</p>
+        {/if}
+    </div>
+</Hoverable>
 
-  <div class="card">
-    <Counter />
-  </div>
+<Hoverable let:hovering={active}>
+    <div class:active>
+        {#if active}
+            <button
+                on:click={() => {
+                    showModal = true;
+                    Set = "Friends";
+                }}
+            >
+                Deep talks
+            </button>
+        {:else}
+            <p>👬 Friends</p>
+        {/if}
+    </div>
+</Hoverable>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
+<Hoverable let:hovering={active}>
+    <div class:active>
+        {#if active}
+            <button
+                on:click={() => {
+                    showModal = true;
+                    Set = "Date";
+                }}
+            >
+                Getting to know you
+            </button>
+        {:else}
+            <p>❤️ Date</p>
+        {/if}
+    </div>
+</Hoverable>
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
+<Hoverable let:hovering={active}>
+    <div class:active>
+        {#if active}
+            <p>👷 Under construction</p>
+        {:else}
+            <p>Input your own</p>
+        {/if}
+    </div>
+</Hoverable>
+
+{#if showModal}
+    <Modal on:close={() => (showModal = false)}>
+        <h2>{getPrompt(Set)}</h2>
+
+        <a href={sources[Set]}>Source</a>
+    </Modal>
+{/if}
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+    div {
+        padding: 1em;
+        margin: 0 0 1em 0;
+        background-color: #eee;
+    }
+
+    .active {
+        background-color: #ff3e00;
+        color: white;
+    }
 </style>
